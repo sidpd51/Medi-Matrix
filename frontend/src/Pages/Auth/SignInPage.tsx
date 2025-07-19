75bec26b65f9e00be5ed8f28180aaf3793af3732
sidpd51 <sidpd510@gmail.com>
2025-07-19 22:39:59 +0530

    refactor: update SignUpPage component

--- frontend/src/Pages/Auth/SignInPage.tsx
+++ frontend/src/Pages/Auth/SignInPage.tsx
@@ -16,17 +16,9 @@ function SignInPage() {
     return (
         <div className="w-full h-screen flex justify-center">
 
-            <Card className="mt-32 w-full max-w-sm max-h-86">
+            <Card className="mt-32 w-full max-w-sm max-h-82">
                 <CardHeader>
-                    <CardTitle>Login to your account</CardTitle>
-                    <CardDescription>
-                        Enter your email below to login to your account
-                    </CardDescription>
-                    <CardAction>
-                        <Button variant="link" asChild>
-                            <Link to="/signup">Sign Up</Link>
-                        </Button>
-                    </CardAction>
+                    <CardTitle className="text-center">Sign In to your account</CardTitle>
                 </CardHeader>
                 <CardContent>
                     <form>
@@ -53,13 +45,14 @@ function SignInPage() {
                                 <Input id="password" type="password" required />
                             </div>
                         </div>
+                        <Button variant="link" className="p-0 mt-3" asChild>
+                            <Link to="/signup">Already have an account ?</Link>
+                        </Button>
+                        <Button type="submit" className="w-full">
+                            Login
+                        </Button>
                     </form>
                 </CardContent>
-                <CardFooter className="flex-col gap-2">
-                    <Button type="submit" className="w-full">
-                        Login
-                    </Button>
-                </CardFooter>
             </Card>
         </div>
     )
