ed5e7599f91680392f3b15decc392a029de89503
sidpd51 <sidpd510@gmail.com>
2025-07-19 22:40:57 +0530

    chor: remove unwanted imports

--- frontend/src/Pages/Auth/SignInPage.tsx
+++ frontend/src/Pages/Auth/SignInPage.tsx
@@ -1,12 +1,9 @@
 import { Button } from "@/components/ui/button"
 import {
     Card,
-    CardAction,
     CardContent,
-    CardDescription,
-    CardFooter,
     CardHeader,
-    CardTitle,
+    CardTitle
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
