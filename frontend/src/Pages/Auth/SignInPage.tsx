740919bbfcda4a9b6c0d55aaaa51005743a040d8
sidpd51 <sidpd510@gmail.com>
2025-07-19 22:14:53 +0530

    feat: add Signin page component

---
+++ frontend/src/Pages/Auth/SignInPage.tsx
@@ -0,0 +1,68 @@
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Link } from "react-router"
+
+function SignInPage() {
+    return (
+        <div className="w-full h-screen flex justify-center">
+
+            <Card className="mt-32 w-full max-w-sm max-h-86">
+                <CardHeader>
+                    <CardTitle>Login to your account</CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account
+                    </CardDescription>
+                    <CardAction>
+                        <Button variant="link" asChild>
+                            <Link to="/signup">Sign Up</Link>
+                        </Button>
+                    </CardAction>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="flex flex-col gap-6">
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email</Label>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    placeholder="m@example.com"
+                                    required
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="password">Password</Label>
+                                    <Link
+                                        to="#"
+                                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                    >
+                                        Forgot your password?
+                                    </Link>
+                                </div>
+                                <Input id="password" type="password" required />
+                            </div>
+                        </div>
+                    </form>
+                </CardContent>
+                <CardFooter className="flex-col gap-2">
+                    <Button type="submit" className="w-full">
+                        Login
+                    </Button>
+                </CardFooter>
+            </Card>
+        </div>
+    )
+}
+
+export default SignInPage;
